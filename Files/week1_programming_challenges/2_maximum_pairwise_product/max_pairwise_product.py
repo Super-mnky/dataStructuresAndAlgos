cbcdae73df5d684886166bac15d9a600ab9d0cc5
Jerry Jones IV <jjonesiv21@gmail.com>
2022-09-26 10:46:23 -0400

    successful fast implmentation

--- Files/week1_programming_challenges/2_maximum_pairwise_product/max_pairwise_product.py
+++ Files/week1_programming_challenges/2_maximum_pairwise_product/max_pairwise_product.py
@@ -1,3 +1,5 @@
+from random import *
+
 def max_pairwise_product(numbers):
     n = len(numbers)
     max_product = 0
@@ -18,8 +20,22 @@ def max_pairwise_productFast(numbers):
     maxprod = sortedNumbers[index1] * sortedNumbers[index2]
     return maxprod
     
-
 if __name__ == '__main__':
-    # _ = int(input())
-    # input_numbers = list(map(int, input().split()))
-    print(max_pairwise_productFast(input_numbers))
+    ''' # there are test cases to try both algos
+
+    for testlimit in range(100):
+        test = []
+        n = randrange(2,100)
+        for i in range(n):
+            test.append(randrange(1,100))
+
+        if (max_pairwise_product(test) == (max_pairwise_productFast(test))):
+            print("ok")
+        else:
+            print(test,max_pairwise_product(test), max_pairwise_productFast(test))
+    
+    '''
+
+    _ = int(input())
+    input_numbers = list(map(int, input().split()))
+    print (max_pairwise_productFast(input_numbers)) 
