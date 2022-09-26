1222b8ba8c5af2671d14278e3243ce8b8c8b0a97
Jerry Jones IV <jjonesiv21@gmail.com>
2022-09-26 10:07:55 -0400

    implemented faster algo, needs tests

--- Files/week1_programming_challenges/2_maximum_pairwise_product/max_pairwise_product.py
+++ Files/week1_programming_challenges/2_maximum_pairwise_product/max_pairwise_product.py
@@ -8,9 +8,18 @@ def max_pairwise_product(numbers):
 
     return max_product
 
+
+input_numbers = [7, 5, 14, 2, 8, 8, 10, 1, 2, 3,]
+
 def max_pairwise_productFast(numbers):
+    sortedNumbers = sorted(numbers, reverse=True) # time complexity of n log n. Uses Timsort algorthim. Hybrid sort method (binInsert + MergeSort)
+    index1 = 0
+    index2 = 1
+    maxprod = sortedNumbers[index1] * sortedNumbers[index2]
+    return maxprod
+    
 
 if __name__ == '__main__':
-    _ = int(input())
-    input_numbers = list(map(int, input().split()))
-    print(max_pairwise_product(input_numbers))
+    # _ = int(input())
+    # input_numbers = list(map(int, input().split()))
+    print(max_pairwise_productFast(input_numbers))
