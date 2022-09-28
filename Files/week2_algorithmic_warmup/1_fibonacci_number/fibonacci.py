f41d9ddb0e11f13394d6c68c4cc31c1886d25d18
Jerry Jones IV <jjonesiv21@gmail.com>
2022-09-28 09:16:17 -0400

    adding fibRecurse_fast

--- Files/week2_algorithmic_warmup/1_fibonacci_number/fibonacci.py
+++ Files/week2_algorithmic_warmup/1_fibonacci_number/fibonacci.py
@@ -1,10 +1,13 @@
-def fibonacci_number(n):
+def fibRecurse(n):
     if n <= 1:
         return n
 
-    return fibonacci_number(n - 1) + fibonacci_number(n - 2)
+    return fibRecurse(n - 1) + fibRecurse(n - 2)
+
+def fibRecurs_fast(n):
+    pass
 
 
 if __name__ == '__main__':
     input_n = int(input())
-    print(fibonacci_number(input_n))
+    print(fibRecurse(input_n))
